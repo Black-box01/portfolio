45775036b652b79dd3a7fbe34a4e255a670d26eb
Kachi <115904975+Black-box01@users.noreply.github.com>
2022-12-04 16:14:20 +0100

    complete file set up

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "Portfolio": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "Portfolio": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3portfoliostorage5d6b1c7f": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
